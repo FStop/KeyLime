018b4cb5b9b0b7e8fd76a4a23af103604d9b9896
Gabriel Luethje <gluethje@email.arizona.edu>
2015-02-05 16:38:37 -0700

    Set up basic Sass structure, move CSS & JS files to /assets

--- functions.php
+++ functions.php
@@ -100,11 +100,9 @@ add_action( 'widgets_init', 'keylime_widgets_init' );
  * Enqueue scripts and styles.
  */
 function keylime_scripts() {
-	wp_enqueue_style( 'keylime-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'keylime-style', get_template_directory_uri() . '/assets/css/main.css');
 
-	wp_enqueue_script( 'keylime-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'keylime-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'keylime-navigation', get_template_directory_uri() . '/assets/js/scripts.js', array(), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
